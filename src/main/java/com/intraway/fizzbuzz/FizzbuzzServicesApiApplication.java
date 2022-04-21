82e6d58da00dafb8a235bfe6a9591813ad75af26
usuario 1 <usuario1@MacBook-Air-de-usuario.local>
2022-04-21 15:32:37 -0300

    initial commit

---
+++ src/main/java/com/intraway/fizzbuzz/FizzbuzzServicesApiApplication.java
@@ -0,0 +1,13 @@
+package com.intraway.fizzbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzServicesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbuzzServicesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intraway/fizzbuzz/FizzbuzzServicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intraway.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzServicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
